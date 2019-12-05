49740ff473817917b56101fc69c4c60fccfb66f7
Victor Emidio Rosado de Paula <victor.paula@energisa.com.br>
2019-12-05 11:31:03 -0300

    adding frontend

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,14 @@ namespace API
 
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -44,6 +52,8 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
