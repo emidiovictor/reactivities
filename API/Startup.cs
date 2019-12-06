65d9e46c86d5a93ddee292056ba9f5157c097110
victor emidio <victoremidio@yahoo.com.br>
2019-12-06 00:47:32 -0300

    Adding CQRS

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,52 +13,44 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistense;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddControllers();
-            services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseCors("CorsPolicy");
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+namespace API {
+  public class Startup {
+    public Startup (IConfiguration configuration) {
+      Configuration = configuration;
     }
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices (IServiceCollection services) {
+
+      services.AddCors (opt => {
+        opt.AddPolicy ("CorsPolicy", policy => {
+          policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
+        });
+      });
+      services.AddMediatR (typeof(List.Handler).Assembly);
+
+      services.AddControllers ();
+      services.AddDbContext<DataContext> (opt => opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection")));
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment ()) {
+        app.UseDeveloperExceptionPage ();
+      }
+
+      app.UseHttpsRedirection ();
+
+      app.UseRouting ();
+
+      app.UseAuthorization ();
+
+      app.UseCors ("CorsPolicy");
+
+      app.UseEndpoints (endpoints => {
+        endpoints.MapControllers ();
+      });
+    }
+  }
 }
